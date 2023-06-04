07e61055e5bfaaf1833c5c8fbf7ce78bd9d5408e
Cfuzuf <cfuzuf@gmail.com>
2023-06-04 13:15:18 +0300

    Добавлена возможность просмотра записей за период и посменно, а также копирование данных в буфер обмена. Добавлен расчет затрат топлива и зарплаты

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.uix.pickers import MDDatePicker
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
+from kivy.core.clipboard import Clipboard
 
-# Размер окна для запуска на ПК. Перед созданием APK файла, удалить или закомментировать.
-from kivy.core.window import Window
-Window.size = (1080/3, 2160/3)
+import platform
+if platform.system() == "Windows" or platform.system() == "Linux":
+    from kivy.core.window import Window
+    Window.size = (1080/3, 2160/3)
 
 
 class MyScreenManager(ScreenManager):
@@ -37,10 +39,15 @@ class EditingRecordInDatabase(Screen):
     pass
 
 
+class ViewingRecordsFromDatabase(Screen):
+    pass
+
+
 class MainApp(MDApp):
-    # settings = {
-    #     "fuel_consumption_per_100_km": "Не настроено"
-    # }
+    settings = {
+        "fuel_consumption_per_100_km": "Не настроено",
+        "work_shift_cost": "Не настроено"
+    }
     day = {
         "date": "Дата",
         "start": "0",
@@ -52,12 +59,12 @@ class MainApp(MDApp):
         "fuel_card_balance": "Не задано..."
     }
 
-    # if not os.path.isfile("./settings.json"):
-    #     with open("./settings.json", "w") as file:
-    #         json.dump(settings, file, indent=4)
-    # else:
-    #     with open("./settings.json", "r") as file:
-    #         settings = json.load(file)
+    if not os.path.isfile("./settings.json"):
+        with open("./settings.json", "w") as file:
+            json.dump(settings, file, indent=4)
+    else:
+        with open("./settings.json", "r") as file:
+            settings = json.load(file)
 
     if not os.path.isfile("./day.json"):
         with open("./day.json", "w") as file:
@@ -66,18 +73,35 @@ class MainApp(MDApp):
         with open("./day.json", "r") as file:
             day = json.load(file)
 
+    def show_date_picker(self, mode):
+        if mode == "day":
+            date_picker = MDDatePicker(
+                title="Выберите сегодняшнюю дату"
+            )
+            date_picker.bind(on_save=self.pick_date)
+            date_picker.open()
+        elif mode == "editing":
+            recording_date = MDDatePicker(
+                title="Выберите дату для редактирования"
+            )
+            recording_date.bind(on_save=self.pick_editing_date)
+            recording_date.open()
+        elif mode == "view":
+            recording_date = MDDatePicker(
+                title="""Выберите дату или диапазон дат
+для просмотра""",
+                mode="range"
+            )
+            recording_date.bind(on_save=self.pick_view_record)
+            recording_date.open()
+
     def pick_date(self, *args):
         date = ".".join(reversed(str(args[1]).split("-")))
         self.root.ids.date.text = date
         self.day["date"] = date
         self.update_day_json()
 
-    def show_date_picker(self):
-        date_picker = MDDatePicker()
-        date_picker.bind(on_save=self.pick_date)
-        date_picker.open()
-
-    def pick_recording_date(self, *args):
+    def pick_editing_date(self, *args):
         date = ".".join(reversed(str(args[1]).split("-")))
         with sqlite3.connect("driving_statistics.db") as db:
             cursor = db.cursor()
@@ -89,13 +113,96 @@ class MainApp(MDApp):
             except Exception as e:
                 print(e)
 
-            if data is not None:
-                self.load_editing_day(data)
-            else:
-                self.show_alert_dialog_record_does_not_exist()
+        if data is not None:
+            self.load_editing_day(data)
+        else:
+            self.show_alert_dialog_record_does_not_exist()
+
+    def pick_view_record(self, *args):
+        self.date_list = [".".join(reversed(str(x).split("-")))
+                          for x in map(str, args[-1][::len(args[-1])-1])]
+
+        with sqlite3.connect("driving_statistics.db") as db:
+            cursor = db.cursor()
+            try:
+                recording = cursor.execute(
+                    """SELECT * FROM my_statistics WHERE date BETWEEN ? AND ?""",
+                    self.date_list
+                )
+                self.viewed_records_list = recording.fetchall()
+            except Exception as e:
+                print(e)
+
+        if self.viewed_records_list:
+            self.load_viewed_records()
+        else:
+            self.show_alert_dialog_record_does_not_exist()
+
+    def load_viewed_records(self):
+        self.root.ids.total_for_period.text = f"За период с {self.date_list[0]} по {self.date_list[1]}"
+        self.root.ids.total_for_period_value_1.text = str(
+            len(self.viewed_records_list))
+        self.root.ids.total_for_period_value_2.text = str(
+            sum(x[3] for x in self.viewed_records_list))
+        self.root.ids.total_for_period_value_3.text = str(
+            sum(x[4] for x in self.viewed_records_list))
+        self.root.ids.total_for_period_value_4.text = str(
+            sum(x[5] for x in self.viewed_records_list))
+
+        self.i = 0
+        self.switch_viewed_record(0)
+
+    def switch_viewed_record(self, i):
+        self.i += i
+
+        self.root.ids.viewing_date.text = self.viewed_records_list[self.i][0]
+        self.root.ids.viewing_start.text = str(
+            self.viewed_records_list[self.i][1])
+        self.root.ids.viewing_stop.text = str(
+            self.viewed_records_list[self.i][2])
+        self.root.ids.viewing_total.text = str(
+            self.viewed_records_list[self.i][3])
+        self.root.ids.viewing_fueling_in_liters.text = str(
+            self.viewed_records_list[self.i][4])
+        self.root.ids.viewing_fueling_in_rubles.text = str(
+            self.viewed_records_list[self.i][5])
+        self.root.ids.viewing_route.text = self.viewed_records_list[self.i][6]
+
+        if self.i == 0:
+            self.root.ids.previous_record.disabled = True
+        else:
+            self.root.ids.previous_record.disabled = False
+
+        if self.i == len(self.viewed_records_list) - 1:
+            self.root.ids.next_record.disabled = True
+        else:
+            self.root.ids.next_record.disabled = False
+
+    def create_message(self, title, text):
+        message = ""
+
+        if self.root.ids.checkbox_viewing_date.active:
+            message += f" Дата: {self.root.ids.viewing_date.text}"
+        if self.root.ids.checkbox_viewing_start.active:
+            message += f" Показания одометра в начале смены: {self.root.ids.viewing_start.text}"
+        if self.root.ids.checkbox_viewing_stop.active:
+            message += f" Показания одометра в конце смены: {self.root.ids.viewing_stop.text}"
+        if self.root.ids.checkbox_viewing_total.active:
+            message += f" Пробег за смену: {self.root.ids.viewing_total.text}"
+        if self.root.ids.checkbox_viewing_consumed_fuel.active:
+            message += f" Израсходовано топлива: {self.root.ids.viewing_consumed_fuel.text}"
+        if self.root.ids.checkbox_viewing_fueling_in_liters.active:
+            message += f" Заправка в литрах: {self.root.ids.viewing_fueling_in_liters.text}"
+        if self.root.ids.checkbox_viewing_fueling_in_rubles.active:
+            message += f" Заправка в рублях: {self.root.ids.viewing_fueling_in_rubles.text}"
+        if self.root.ids.checkbox_viewing_route.active:
+            message += f" Маршрут: {self.root.ids.viewing_route.text}"
+
+        Clipboard.copy(message.strip())
 
     def load_editing_day(self, data_list):
         self.data_list = list(map(str, data_list))
+
         (self.root.ids.editing_date.text,
          self.root.ids.editing_start.text,
          self.root.ids.editing_stop.text,
@@ -137,14 +244,9 @@ class MainApp(MDApp):
 
         self.root.ids.editing_date.text = ""
 
-    def show_recording_date_selection(self):
-        recording_date = MDDatePicker()
-        recording_date.bind(on_save=self.pick_recording_date)
-        recording_date.open()
-
-    # def update_settings_json(self):
-    #     with open("./settings.json", "w") as file:
-    #         json.dump(self.settings, file, indent=4)
+    def update_settings_json(self):
+        with open("./settings.json", "w") as file:
+            json.dump(self.settings, file, indent=4)
 
     def update_day_json(self):
         with open("./day.json", "w") as file:
@@ -167,23 +269,15 @@ class MainApp(MDApp):
 
         self.update_day_json()
 
-    # def calculation_and_update_fuel_consumed(self):
-    #     if self.settings["fuel_consumption_per_100_km"] == "Не настроено":
-    #         self.day["fuel_consumed"] = "Не настроено"
-    #         self.root.ids.fuel_consumed.text = self.day["fuel_consumed"]
-    #     else:
-    #         if self.day["total"] == " ":
-    #             self.day["fuel_consumed"] = "0"
-    #             self.root.ids.fuel_consumed.text = self.day["fuel_consumed"]
-    #         else:
-    #             fuel_consumed = str(
-    #                 int(self.day["total"]) *
-    #                 int(self.settings["fuel_consumption_per_100_km"]) /
-    #                 100
-    #             )
-    #             self.day["fuel_consumed"] = fuel_consumed
-    #             self.root.ids.fuel_consumed.text = self.day["fuel_consumed"]
-
+    def calculation_and_update_fuel_consumed(self, num):
+        if self.settings["fuel_consumption_per_100_km"] == "Не настроено":
+            return "0"
+        else:
+            if num:
+                return str(int(num) * int(self.settings["fuel_consumption_per_100_km"]) / 100)
+            else:
+                return "0"
+        
     def update_fueling(self, id, text):
         if text:
             self.day[id] = str(int(self.day[id]) + int(text))
@@ -198,24 +292,31 @@ class MainApp(MDApp):
         self.day["route"] = " ".join(args)
         self.update_day_json()
 
-    # def setting_fuel_consumption_per_100_km(self, text):
-    #     if text:
-    #         self.settings["fuel_consumption_per_100_km"] = str(text)
-    #         self.update_settings_json()
-    #     else:
-    #         self.root.ids.fuel_consumption_per_100_km.text = self.settings[
-    #             "fuel_consumption_per_100_km"
-    #         ]
+    def setting_fuel_consumption_per_100_km(self, text):
+        if text:
+            self.settings["fuel_consumption_per_100_km"] = str(text)
+            self.update_settings_json()
+        else:
+            self.root.ids.fuel_consumption_per_100_km.text = self.settings[
+                "fuel_consumption_per_100_km"
+            ]
 
-    # def setting_fuel_card_balance(self, text):
-    #     if text:
-    #         self.day["fuel_card_balance"] = str(text)
-    #         self.root.ids.settings_fuel_card_balance.text = self.day["fuel_card_balance"]
-    #         self.root.ids.fuel_card_balance.text = self.day["fuel_card_balance"]
-    #         self.update_day_json()
-    #     else:
-    #         self.root.ids.settings_fuel_card_balance.text = self.day["fuel_card_balance"]
-    #         self.root.ids.fuel_card_balance.text = self.day["fuel_card_balance"]
+    def setting_work_shift_cost(self, text):
+        if text:
+            self.settings["work_shift_cost"] = str(text)
+            self.root.ids.work_shift_cost.text = self.settings["work_shift_cost"]
+            self.update_settings_json()
+        else:
+            self.root.ids.work_shift_cost.text = self.settings["work_shift_cost"]
+
+    def calculation_salary(self, number_work_shifts):
+        if self.settings["work_shift_cost"] == "Не настроено":
+            return "0"
+        else:
+            if number_work_shifts:
+                return str(int(self.settings["work_shift_cost"]) * int(number_work_shifts))
+            else:
+                return "0"
 
     def calculation_fuel_card_balance(self, last_fueling):
         if self.day["fuel_card_balance"] == "Не задано...":
@@ -250,7 +351,6 @@ class MainApp(MDApp):
             buttons=[
                 MDFlatButton(
                     text="Понятно",
-                    # text_color=self.theme_cls.primary_color,
                     on_press=self.close_dialog
                 )
             ],
@@ -264,7 +364,6 @@ class MainApp(MDApp):
             buttons=[
                 MDFlatButton(
                     text="Ясно",
-                    # text_color=self.theme_cls.primary_color,
                     on_press=self.close_dialog
                 )
             ],
